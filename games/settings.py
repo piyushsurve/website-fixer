670568db053e51f790d370f8d2d9ff508a5d2b84
piyushsurve <142381118+piyushsurve@users.noreply.github.com>
2026-08-02 14:03:25 -0700

    Update settings.py
    Database Configurations are changed

--- games/settings.py
+++ games/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'games.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
